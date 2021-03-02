43410a15ac5a2b2b40eed2178846bd9fb18f7b6b
fabionardelli <fabionardelli93@gmail.com>
2021-03-02 14:38:55 +0100

    improved draw method in decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -2,64 +2,6 @@ from core import get_solutions
 from dataclasses import dataclass
 import pydot
 
-'''
-class Node:
-    def __init__(self, value):
-        self.value = value
-
-    def get_value(self):
-        return self.value
-
-
-class BinTree:
-    root = 0
-
-    def __init__(self, nodes):
-        self.num_nodes = len(nodes)
-        self.nodes = nodes
-
-    def get_root(self):
-        return self.root
-
-    def left_child(self, n):
-        try:
-            return self.nodes[2 * n + 1]
-        except IndexError as e:
-            print(e)
-
-    def right_child(self, n):
-        try:
-            return self.nodes[2 * n + 2]
-        except IndexError as e:
-            print(e)
-            
-            
-
-
-n1 = Node('a')
-n2 = Node('b')
-n3 = Node('c')
-
-# t = BinTree([n1, n2, n3])
-# print(t.left_child(t.get_root()))
-solutions = get_solutions()
-for i, s in enumerate(solutions):
-    g = pydot.Dot('t%i' % i, graph_type='digraph')
-    v_var = s['v']
-    for k in v_var:
-        g.add_node(pydot.Node('%s' % k, shape='circle'))
-
-    l_var = s['l']
-    for k, v in l_var.items():
-        g.add_edge(pydot.Edge(k, v, color='black'))
-
-    r_var = s['r']
-    for k, v in r_var.items():
-        g.add_edge(pydot.Edge(k, v, color='black'))
-
-    g.write_png('t%i.png' % i)
-'''
-
 
 @dataclass
 class Node:
@@ -171,15 +113,22 @@ class DecisionTree:
         """
         g = pydot.Dot('tree', graph_type='digraph')
 
+        visited = set()  # already visited nodes
+
         # Scan each node in the tree
         for node in self.tree_structure:
-            # If the node is not a leaf...
-            if self.tree_structure[node] is not None:
-                # Get its left and right children...
+            # If the node is not a leaf and has not been visited yet...
+            if self.tree_structure[node] is not None and node not in visited:
+                # add it to the pydot graph...
+                g.add_node(pydot.Node('%s' % node, shape='circle'))
+                # and to the visited set
+                visited.add(node)
+
+                # get its left and right children...
                 left_child = self.tree_structure[node][0]
                 right_child = self.tree_structure[node][1]
 
-                # And add them to the pydot graph
+                # and add them to the pydot graph
                 g.add_node(pydot.Node('%s' % left_child, shape='circle'))
                 g.add_node(pydot.Node('%s' % right_child, shape='circle'))
 
